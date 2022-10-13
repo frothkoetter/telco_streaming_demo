a7d0388f47f8575dc538ad0c1531a3961ccd9fc1
Friedel Rothkötter <frothkoetter@cloudera.com>
2022-10-13 13:33:55 +0200

    removed files

--- gen-burst.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-host=telco-demo-events-nifi2
-
-while [ 1 ] 
-do 
- python3 gen-events.py -nc 1 -iso 'de' -state 'Germany' -r 'LTE' -ne 2000 -nd '1m' -f 'cell-tower.csv' 
- sleep 275 
-done
